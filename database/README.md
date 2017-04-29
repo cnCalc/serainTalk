99cbb31a4764e7e9dca67f89f14203fde6c4dfb4
ntzyz <ntzyz@live.cn>
2017-04-29 17:15:56 +0000

    Add db create script.

---
+++ database/README.md
@@ -0,0 +1,10 @@
+Database
+========
+
+Initial state of database for serainTalk.
+
+## Install
+
+```
+mongo localhost:27017/cncalc ./script.js
+```
---
+++ database/script.js
@@ -0,0 +1,12 @@
+db.generic.remove({});
+db.generic.insertMany([{
+  key: 'pinned-tags',
+  tags: [
+    'Hewlett Packard',
+    'CASIO',
+    'Ti',
+    '图形/编程机',
+    '函数机',
+    '程序与游戏',
+  ]
+}]);
