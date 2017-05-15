71537f4e1ebe1ac8dee354aadbf0ab3163284f41
ntzyz <ntzyz@live.cn>
2017-05-15 19:09:07 +0000

    Update 404 page.

--- web/src/components/NotFound.vue
+++ web/src/components/NotFound.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   div.not-found
-    h1 Oops
+    h1 :(
     h2 The page you are looking for could not be found.
 </template>
 
@@ -13,8 +13,13 @@ export default {
 <style lang="scss">
 div.not-found {
   padding: 80px;
+  text-align: left;
   h1, h2 {
     font-weight: normal;
+    padding: 20px;
+  }
+  h1 {
+    font-size: 48px;
   }
 }
 </style>
