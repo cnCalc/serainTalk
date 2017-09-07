052192de4a0504561eb4d5da8ee24db96abfa283
ntzyz <ntzyz@live.cn>
2017-09-07 20:15:38 +0800

    [修复] 修正了一个在用户页跳转至其他用户页不刷新的问题。

--- web/src/views/MemberView.vue
+++ web/src/views/MemberView.vue
@@ -76,12 +76,14 @@ export default {
   },
   watch: {
     '$route': function (route) {
-      if ((route.meta.mode === 'discussions' && this.currentMember !== route.params.memberId) || this.firstIn) {
-        this.firstIn = false;
-        this.$store.dispatch('fetchDiscussionsCreatedByMember', { id: route.params.memberId });
-        this.currentPage = 1;
+      if (typeof this.currentMember === 'undefined' || this.currentMember !== route.params.memberId || this.firstIn) {
+        // 如果 currentMember 和路由中的 member 不一致时，显然可以算作是首次进入。
+        let needRefetchMemberInfo = (this.currentMember !== route.params.memberId);
         if (route.params.memberId) {
           this.currentMember = route.params.memberId;
+          needRefetchMemberInfo && this.$store.dispatch('fetchMemberInfo', { id: route.params.memberId });
+          this.$store.dispatch('fetchDiscussionsCreatedByMember', { id: route.params.memberId });
+          this.currentPage = 1;
         }
       }
     }
