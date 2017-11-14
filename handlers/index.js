905849053ee7df2851d72ebecd89bea0ada17a73
kasora <kasorasun@gmail.com>
2017-11-14 15:52:19 +0800

    [修复] 错误处理 - 只有字段验证错误才会产生400 - 其余错误依旧显示为500-server error

--- handlers/index.js
+++ handlers/index.js
@@ -8,7 +8,11 @@ const errorMessages = require('../utils/error-messages');
 router.use('/v1', require('./v1'));
 
 router.use((err, req, res, next) => {
-  return errorHandler(null, errorMessages.VALIDATION_ERROR, 400, res);
+  if (err.message === 'validation error') {
+    return errorHandler(null, errorMessages.VALIDATION_ERROR, 400, res);
+  } else {
+    return errorHandler(err, errorMessages.SERVER_ERROR, 500, res);
+  }
 });
 
 module.exports = router;
