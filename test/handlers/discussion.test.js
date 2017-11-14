af62187c02f982c16dcd1159027915658f3414a5
kasora <kasorasun@gmail.com>
2017-11-14 15:40:15 +0800

    [修复] 移除测试中的console.log

--- test/handlers/discussion.test.js
+++ test/handlers/discussion.test.js
@@ -19,15 +19,14 @@ describe('discussion part', async () => {
   it('get latest discussion list by memberid.', async () => {
     await testTools.member.createOneMember(agent, async (newMemberInfo) => {
       let url = `/api/v1/discussions/latest?${newMemberInfo.id}`;
-      try {
-        let discussionList = await agent
-          .get(url)
-          .expect(200);
-        expect(discussionList.body.status).to.be.equal('ok');
-        discussionList = discussionList.body;
-      } catch (err) {
-        console.log(err);
-      }
+      let discussionList = await agent
+        .get(url)
+        .expect(200);
+      expect(discussionList.body.status).to.be.equal('ok');
+      discussionList = discussionList.body;
+
+      // FIXME 补充测试
+      // console.log(discussionList);
     });
   });
 
@@ -55,7 +54,7 @@ describe('discussion part', async () => {
           let postInfo = postRes.body.newPost;
           expect(postInfo.index).to.be.ok;
           // FIXME 检查数据是否成功添加
-          console.log(postRes);
+          // console.log(postRes);
         });
       });
     });
