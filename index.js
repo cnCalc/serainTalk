396e479a9bebfb645898ccbb15083bf3bf4161b9
kasora <kasorasun@gmail.com>
2017-11-14 15:40:47 +0800

    [修复] 移除不使用的引入

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const validation = require('express-validation');
-const errorHandler = require('./utils/error-handler');
 
 const app = express();
 
