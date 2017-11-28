fb26543e725b14e5064d4a737072dd0cb28ada6a
kasora <kasorasun@gmail.com>
2017-11-28 21:46:05 +0800

    [新增] 获取 message - 按 memberId 获取 - 按 messageId 获取 - 对应测试

--- test/handlers/message.test.js
+++ test/handlers/message.test.js
@@ -36,7 +36,7 @@ describe('message part', async () => {
       let messageRes = await agent.get(getUrl)
         .expect(200);
       messageRes = messageRes.body;
-      let messages = messageRes.messages;
+      let messages = messageRes.messagesInfo;
       let count = messageRes.count;
       expect(messages).to.be.an('array');
       expect(messages[0]).to.be.ok;
@@ -44,6 +44,61 @@ describe('message part', async () => {
       expect(messages[0].participatesInfo).to.be.an('array');
       expect(messages[0].participatesInfo.length).to.be.equal(2);
       expect(count).to.be.ok;
+    });
+  });
+
+  it('get message by memberId', async () => {
+    await testTools.member.createOneMember(agent, null, async (newMemberInfoA) => {
+      await testTools.member.createOneMember(agent, null, async (newMemberInfoB) => {
+        let postUrl = `/api/v1/message/${newMemberInfoA.id}`;
+        let payload = {
+          content: 'hello, here is test message.'
+        };
+        for (let i = 0; i < config.pagesize + 1; i++) {
+          await agent.post(postUrl)
+            .send(payload)
+            .expect(201);
+        }
+        let getUrl = `/api/v1/messages/member/${newMemberInfoA.id}`;
+        let messageRes = await agent.get(getUrl)
+          .expect(200);
+        let message = messageRes.body.message;
+        let count = messageRes.body.count;
+        expect(message.timeline).to.be.an('array');
+        expect(message.timeline.length).to.be.equal(config.pagesize);
+        expect(Object.keys(message.timeline[0])).to.include.members(['from', 'date', 'content']);
+        expect(count).to.be.equal(config.pagesize + 1);
+      });
+    });
+  });
+
+  it('get message by messageId', async () => {
+    await testTools.member.createOneMember(agent, null, async (newMemberInfoA) => {
+      await testTools.member.createOneMember(agent, null, async (newMemberInfoB) => {
+        let postUrl = `/api/v1/message/${newMemberInfoA.id}`;
+        let payload = {
+          content: 'hello, here is test message.'
+        };
+        for (let i = 0; i < config.pagesize + 1; i++) {
+          await agent.post(postUrl)
+            .send(payload)
+            .expect(201);
+        }
+        let getUrl = '/api/v1/messages';
+        let messageRes = await agent.get(getUrl)
+          .expect(200);
+        let messageInfo = messageRes.body.messagesInfo[0];
+
+        getUrl = `/api/v1/message/${messageInfo._id}?page=2`;
+        messageRes = await agent.get(getUrl)
+          .expect(200);
+        let message = messageRes.body.message;
+        let count = messageRes.body.count;
+        expect(message.timeline).to.be.an('array');
+        expect(message.timeline.length).to.be.equal(1);
+        expect(Object.keys(message.timeline[0])).to.include.members(['from', 'date', 'content']);
+        expect(count).to.be.equal(config.pagesize + 1);
+      });
     });
   });
 });
